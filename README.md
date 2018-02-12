661ed783a299a8c32906b6bcb16ba45f8356c3fe
efriedla <efriedla20852@gmail.com>
2018-02-12 15:44:00 -0800

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+![Alt text](http://g.recordit.co/u6YviXaelo.gif)
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+class App extends Component {
+  
+  render() {
+    const url = 'https://jsonplaceholder.typicode.com/photos';
+    // const doubled = url.map((photo) => console.log(photo));
+  
+    
+      
+    return (
+      <div >
+        <h1>My library</h1>
+       
+        
+        
+      </div>
+    );
+  }
+}
+
+export default App;
