cecc4ba11ce04d0c301190f2f6a7865ab54427b5
efriedla <efriedla20852@gmail.com>
2018-02-12 16:07:32 -0800

    imported reducers, Provider, and middleware

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('#root'));
