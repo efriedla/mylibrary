49ab7d7a03c442af1a991d325c5f633788254d15
efriedla <efriedla20852@gmail.com>
2018-02-12 16:18:21 -0800

    added a function in the reducer containing books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: "javascript: the good parts"},
+        {title: "Harry Potter"},
+        {title: "Black Beauty"},
+        {title: "frankenstein"}
+    ]
+}
