9a27c23e862cabeea5674a21b4b8b94a63222a27
efriedla <efriedla20852@gmail.com>
2018-02-14 11:56:31 -0800

    grabbing type and book from action/index

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,8 +1,15 @@
 /**
  * 
  * @param {*} state argument is not application state, only the state
+ * state has to have defaul of null for redux cannot be undefined
+ * case is the type in action index
+ * book is the book in action index
  * @param {*} action 
  */
-export default function(state, action){
-
+export default function(state = null, action){
+    switch(action.type){
+        case 'Book_Selected':
+        return action.book
+    }
+    return state
 }
